fe73cc9b0d8bede946f2a9d626d52271f330db0d
max.marchuk <maksim.marchuk@ticketmaster.com>
2017-11-21 00:15:47 -0800

    badge roration on timer tick added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      secondsElapsed: 0
+      secondsElapsed: 0,
+      currentBadgeUrl: 'https://s3-us-west-2.amazonaws.com/honda-badge-images/parking-permit-1.png',
+      CURRENT_BADGE_RETRIEVE_URL: 'https://server.test-cors.org/server?id=1714196&enable=true&status=200&credentials=false',
     };
-    debugger;
   }
 
+  timeTick(context) {
+    context.setState((prevState, props) => {
+      return {secondsElapsed: context.state.secondsElapsed + 1};
+    });
+  }
+
+  imageFetchTick(context) {
+    return fetch(context.state.CURRENT_BADGE_RETRIEVE_URL)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        context.setState({
+          currentBadgeUrl: context.state.currentBadgeUrl.replace(/\d*.png/gi, (Math.floor(Math.random() * 2) + 1) + '.png')  //responseJson.response.url,
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -20,7 +41,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="https://s3-us-west-2.amazonaws.com/honda-badge-images/parking-permit-example.png" className="App-logo" alt="logo" />
+          <img src={this.state.currentBadgeUrl} className="App-logo" alt="logo" />
           <h1 className="App-title">THIS IS PAAARKING!!!</h1>
           <h2>{this.state.secondsElapsed}</h2>
         </header>
@@ -29,22 +50,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-    setInterval(() => this.setState({ secondsElapsed: this.state.secondsElapsed + 1}), 1000)
-    // return fetch('https://facebook.github.io/react-native/movies.json')
-    //   .then((response) => response.json())
-    //   .then((responseJson) => {
-    //     let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    //     this.setState({
-    //       isLoading: false,
-    //       dataSource: ds.cloneWithRows(responseJson.movies),
-    //     }, function() {
-    //       // do something with new state
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
+    this.interval = setInterval(this.timeTick, 1000, this);
+    this.interval = setInterval(this.imageFetchTick, 2 * 1000, this);
   }
 }
 
